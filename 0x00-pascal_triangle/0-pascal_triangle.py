978b123f53bab46eda1afc2038baf272f5095185
Reydigital <regysmarie@gmail.com>
2022-12-23 02:21:12 +0000

    commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,29 +1,64 @@
 #!/usr/bin/python3
 
-"""pascal_triangle module.
-Contains a function that returns a list of lists of
-integers representing the Pascal’s triangle of n.
 """
+
+Defines function that returns a list of lists of integers
+
+representing the Pascal's triangle of n
+
+"""
+
 
 def pascal_triangle(n):
 
         """
-            Returns a list of lists of integers
-                representing the Pascal’s triangle of n.
-                    """
 
-                        if n <= 0:
-                                    return []
+            Creates a list of lists of integers representing Pascal's triangle
 
-                                    result = [[1]]
+                parameters:
 
-                                        while len(result) is not n:
+                        n [int]:
 
-                                                    tmp = [1]
+             the number of rows of Pascal's triangle to recreate
 
-                               for i in range(len(result[-1]) - 1):
+                                        return:
 
-                     tmp.append(result[-1][i] + result[-1][i + 1])
-                                                                                    tmp.append(1)
-                                                                                            result.append(tmp)
-                                                                                                return result
+                                                [list of lists of ints]:                                representation of Pascal's triangle
+
+                                                                """
+
+                             if type(n) is not int:
+
+                                                                                raise TypeError("n must be an integer")
+
+                                                                                triangle = []
+
+                                                                                    if n <= 0:
+
+                                                                                                return triangle
+
+                                                                                                previous = [1]
+
+                                                                                                    for row_index in range(n):
+
+                                                                                                                rowlist = []
+
+                                                                                                                        if row_index == 0:
+
+                                                                                                                                        rowlist = [1]
+
+                                                                                                                                                else:
+
+                                                                                                                                                                for i in range(row_index + 1):
+
+                                                                                                                                                                                    if i == 0:
+
+                                                                                                                                                                                                            rowlist.append(0 + previous[i])
+                                                                                                                                                                               elif i == (row_index):
+
+                                                                                                                                                                                                                                                    rowlist.append(previous[i - 1] + 0)
+
+                                                                                                                                                                     else:                                                                                                                                                                                                                                                      rowlist.append(previous[i - 1] + previous[i])
+                                                                                                                                                                     previous = rowlist
+                                                                                                                                                                                                                                                 triangle.append(rowlist)
+                                                                                                                                                                                                                                                return triangle
